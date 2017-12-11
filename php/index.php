318aec538d5bb9d1eb024167b0c4cbaea9cc1bb3
Petar Prodanovic <petar.prodanovic@gmail.com>
2017-12-11 16:25:01 +0100

    no message

---
+++ php/index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: petar
+ * Date: 8.12.2017
+ * Time: 14:35
+ */
+
+?>
+<html>
+
+<head>
+    <title>Hello World</title>
+</head>
+
+<body>
+<?php echo "Hello, World!";?>
+</body>
+
+</html>
